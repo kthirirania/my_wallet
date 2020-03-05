b3687c7d71e4a64edcd598c50b8cd48c392c50e0
Rania Kthiri <rania.kthiri@isetrades.com>
2020-03-05 14:29:44 +0100

    adding assets, shimmer and cached image libs commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        //I have set Safe area top to false in the bottom_navigation_widget
+        // so I need to make a space for it here.
+        SizedBox(
+          height: 64,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              SvgPicture.asset(
+                'assets/icons/menu.svg',
+                color: Colors.black,
+                width: 24,
+                height: 24,
+              ),
+              Text(
+                "Virtual Card",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Playfair',
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
