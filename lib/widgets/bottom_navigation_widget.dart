44426010abc19c0a59470dd0ba6c240455c5fec0
Rania Kthiri <rania.kthiri@isetrades.com>
2020-03-05 13:45:12 +0100

    circular bottom nav lib commit

---
+++ lib/widgets/bottom_navigation_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationWidget extends StatefulWidget {
+  @override
+  _BottomNavigationWidgetState createState() => _BottomNavigationWidgetState();
+}
+
+class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
