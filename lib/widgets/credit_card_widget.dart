bbf42542f0f5cf8dfbd1b84b981c193790307aa9
Rania Kthiri <rania.kthiri@isetrades.com>
2020-03-06 18:19:53 +0100

    transactions list format commit

--- lib/widgets/credit_card_widget.dart
+++ lib/widgets/credit_card_widget.dart
@@ -1,7 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:local_auth/local_auth.dart';
 
-class CreditCardWidget extends StatelessWidget {
+class CreditCardWidget extends StatefulWidget {
+  @override
+  _CreditCardWidgetState createState() => _CreditCardWidgetState();
+}
+
+class _CreditCardWidgetState extends State<CreditCardWidget> {
+  final LocalAuthentication auth = LocalAuthentication();
+  List<BiometricType> _availableBiometrics = [];
+  bool _canCheckBiometrics = false;
+  bool _authorized = false;
+  bool _isAuthenticating = false;
+
+  Future<void> _checkBiometrics() async {
+    if (!_authorized) {
+      bool canCheckBiometrics;
+      try {
+        canCheckBiometrics = await auth.canCheckBiometrics;
+      } on PlatformException catch (e) {
+        print(e);
+      }
+      if (!mounted) return;
+
+      setState(() {
+        _canCheckBiometrics = canCheckBiometrics;
+      });
+    }
+  }
+
+  Future<void> _getAvailableBiometrics() async {
+    if (!_authorized && _canCheckBiometrics) {
+      List<BiometricType> availableBiometrics;
+      try {
+        availableBiometrics = await auth.getAvailableBiometrics();
+      } on PlatformException catch (e) {
+        print(e);
+      }
+      if (!mounted) return;
+
+      setState(() {
+        _availableBiometrics = availableBiometrics;
+      });
+    }
+  }
+
+  Future<void> _authenticate() async {
+    if (!_authorized && _availableBiometrics.length > 0) {
+      bool authenticated = false;
+      try {
+        setState(() {
+          _isAuthenticating = true;
+        });
+        authenticated = await auth.authenticateWithBiometrics(
+            localizedReason: 'Scan your fingerprint to authenticate',
+            useErrorDialogs: true,
+            stickyAuth: true);
+        setState(() {
+          _isAuthenticating = false;
+        });
+      } on PlatformException catch (e) {
+        print(e);
+      }
+      if (!mounted) return;
+
+      setState(() {
+        _authorized = authenticated ? true : false;
+      });
+    } else {
+      setState(() {
+        _authorized = false;
+      });
+    }
+  }
+
+  void _cancelAuthentication() {
+    setState(() {
+      _authorized = false;
+    });
+    auth.stopAuthentication();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -62,12 +143,23 @@ class CreditCardWidget extends StatelessWidget {
                         color: Color(0x33ffffff),
                         borderRadius: BorderRadius.circular(12.0),
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: SvgPicture.asset(
-                          'assets/icons/fingerprint.svg',
-                          height: 32,
-                          color: Colors.white,
+                      child: InkWell(
+                        onTap: () async {
+                          await _checkBiometrics();
+                          await _getAvailableBiometrics();
+                          _isAuthenticating
+                              ? _cancelAuthentication()
+                              : await _authenticate();
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: SvgPicture.asset(
+                            _authorized
+                                ? 'assets/icons/lock.svg'
+                                : 'assets/icons/fingerprint.svg',
+                            height: 32,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
@@ -82,7 +174,7 @@ class CreditCardWidget extends StatelessWidget {
                   mainAxisSize: MainAxisSize.max,
                   children: <Widget>[
                     Text(
-                      "* * * *",
+                      _authorized ? "1254" : "* * * *",
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 20,
@@ -102,7 +194,7 @@ class CreditCardWidget extends StatelessWidget {
                       ),
                     ),
                     Text(
-                      "* * * *",
+                      _authorized ? "4456" : "* * * *",
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 20,
@@ -122,7 +214,7 @@ class CreditCardWidget extends StatelessWidget {
                       ),
                     ),
                     Text(
-                      "* * * *",
+                      _authorized ? "0045" : "* * * *",
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 20,
@@ -178,28 +270,50 @@ class CreditCardWidget extends StatelessWidget {
                     ),
                   ),
                   Padding(
-                      padding: const EdgeInsets.only(left: 12.0),
-                      child: Text(
-                        "\$ 26,848.00",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          shadows: <Shadow>[
-                            Shadow(
-                              offset: Offset(1.0, 1.0),
-                              blurRadius: 2.0,
-                              color: Colors.black26,
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: _authorized
+                        ? Text(
+                            "\$ 26,848.00",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              shadows: <Shadow>[
+                                Shadow(
+                                  offset: Offset(1.0, 1.0),
+                                  blurRadius: 2.0,
+                                  color: Colors.black26,
+                                ),
+                                Shadow(
+                                  offset: Offset(1.0, 1.0),
+                                  blurRadius: 2.0,
+                                  color: Colors.black26,
+                                ),
+                              ],
                             ),
-                            Shadow(
-                              offset: Offset(1.0, 1.0),
-                              blurRadius: 2.0,
-                              color: Colors.black26,
+                          )
+                        : Text(
+                            "\$\$\$,\$\$\$.\$\$",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              shadows: <Shadow>[
+                                Shadow(
+                                  offset: Offset(1.0, 1.0),
+                                  blurRadius: 2.0,
+                                  color: Colors.black26,
+                                ),
+                                Shadow(
+                                  offset: Offset(1.0, 1.0),
+                                  blurRadius: 2.0,
+                                  color: Colors.black26,
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                      )),
+                          ),
+                  ),
                 ],
               ),
               Row(
