8d09eb66ea9195ca0f7ff83c61c5d9122b421c3e
Rania Kthiri <rania.kthiri@isetrades.com>
2020-03-12 13:56:14 +0100

    transactions list format commit

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -10,46 +10,46 @@ class Transaction {
     return [
       new Transaction(
           image:
-              "https://drive.google.com/uc?export=view&id=1xicySkjb5Kk3x5pLcgLnGBPyT9oce6kS",
+              "https://drive.google.com/uc?export=view&id=1GiWdgZjgCu41sJM7okEXgr7j5gC1BpDl",
           name: "Sofa",
           date: "2020-06-12 12:29",
-          amount: 1.50),
+          amount: 1000.50),
       new Transaction(
           image:
-              "https://drive.google.com/uc?export=view&id=14BxGUM0sc5c6K7d9exfABu_sHB98EOEg",
+              "https://drive.google.com/uc?export=view&id=1_VRbb2IGhC05n6T6jPQBmU8-APXinf7p",
           name: "Twin Bed",
           date: "2020-06-12 12:25",
-          amount: 180.50),
+          amount: 2080.50),
       new Transaction(
           image:
-              "https://drive.google.com/uc?export=view&id=1BCvf5oivKaKQeKTotPbxnn1Evyzx3nMM",
+              "https://drive.google.com/uc?export=view&id=1H-2UrY3HCflOebkSwnnTM8aMr8Bs39jB",
           name: "Donut",
           date: "2020-06-12 08:30",
           amount: 2.5),
       new Transaction(
           image:
-              "https://drive.google.com/uc?export=view&id=1kMTt074dvqcqglNgPP1DZj8VlE-YKrAT",
+              "https://drive.google.com/uc?export=view&id=1bNVPTnVyHzjB1KSd0zpc3DXtupS9KiC4",
           name: "Croissant",
           date: "2020-06-12 09:04",
           amount: 1.50),
       new Transaction(
           image:
-              "https://drive.google.com/uc?export=view&id=1UNLCE9WXm4QBftfCAKun5YIbyIS3LKSU",
+              "https://drive.google.com/uc?export=view&id=173k4Jr5NaEeJ6perYnfXtKSCZ58aKX0l",
           name: "Coffee",
           date: "2020-06-12 09:08",
           amount: 1.5),
       new Transaction(
           image:
-              "https://drive.google.com/uc?export=view&id=1fHaAcwXgJQzsG8VVgtuwrTXcwaF-z2kY",
+              "https://drive.google.com/uc?export=view&id=1HlFylg9l2sqWp3iTzX_7QnlYS3WSa_bM",
           name: "Air Conditionner",
           date: "2020-06-12 12:30",
           amount: 1080.0),
       new Transaction(
           image:
-              "https://drive.google.com/uc?export=view&id=1hy2o-DTCnpZauw379DyPapersrNtDiBI",
+              "https://drive.google.com/uc?export=view&id=12eEPmXNDkbXauNRTbrnmoZcRMfEIOKPJ",
           name: "Pizza",
           date: "2020-06-12 12:29",
-          amount: 1.50),
+          amount: 6.50),
     ];
   }
 }
